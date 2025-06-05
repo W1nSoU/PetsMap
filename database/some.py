734ebf39de65d7e3b21a16737d3b8557ecb7481c
RecognizesPain <knung.namba.van@gmail.com>
2025-06-06 02:19:46 +0300

    Почистив бд

--- database/some.py
+++ database/some.py
@@ -1,29 +1,33 @@
 import sqlite3
 
 # Підключення до бази даних (створення файлу, якщо його не існує)
-conn = sqlite3.connect('D:/PetsMap/PetsMap/database/animals.db')
+conn = sqlite3.connect('D:/PetsMap/PetsMap/database/users.db')
 cursor = conn.cursor()
 
 # Створення таблиці для запису даних про тварин
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS animals (
-    id INTEGER PRIMARY KEY AUTOINCREMENT, -- Унікальний ідентифікатор
-    name TEXT NOT NULL, -- Ім'я тваринки
-    type TEXT NOT NULL, -- Тип тваринки (Собака, Кіт, Птах, Гризун, Інше)
-    location TEXT NOT NULL, -- Місцезнаходження (Київ, Львів, Івано-Франківськ)
-    reason TEXT NOT NULL, -- Причина здачі (Переїзд, Алергія, Фінансові труднощі, Знайдено на вулиці, Інше)
-    age INTEGER NOT NULL, -- Вік тваринки
-    gender TEXT NOT NULL, -- Стать (Хлопчик, Дівчинка)
-    breed TEXT NOT NULL, -- Порода
-    sterilization TEXT NOT NULL, -- Стерилізований (Так, Ні)
-    vaccination TEXT NOT NULL, -- Щеплення (Так, Ні, Не знаю)
-    microchip TEXT NOT NULL, -- Мікрочіп (Так, Ні)
-    vet_passport TEXT NOT NULL, -- Ветеринарний паспорт (Так, Ні)
-    image_path TEXT NOT NULL, -- Шлях до фотографії тваринки
-    description TEXT NOT NULL -- Інформація про тваринку
-)
-''')
+# cursor.execute('''
+# CREATE TABLE IF NOT EXISTS animals (
+#     id INTEGER PRIMARY KEY AUTOINCREMENT, -- Унікальний ідентифікатор
+#     name TEXT NOT NULL, -- Ім'я тваринки
+#     type TEXT NOT NULL, -- Тип тваринки (Собака, Кіт, Птах, Гризун, Інше)
+#     location TEXT NOT NULL, -- Місцезнаходження (Київ, Львів, Івано-Франківськ)
+#     reason TEXT NOT NULL, -- Причина здачі (Переїзд, Алергія, Фінансові труднощі, Знайдено на вулиці, Інше)
+#     age INTEGER NOT NULL, -- Вік тваринки
+#     gender TEXT NOT NULL, -- Стать (Хлопчик, Дівчинка)
+#     breed TEXT NOT NULL, -- Порода
+#     sterilization TEXT NOT NULL, -- Стерилізований (Так, Ні)
+#     vaccination TEXT NOT NULL, -- Щеплення (Так, Ні, Не знаю)
+#     microchip TEXT NOT NULL, -- Мікрочіп (Так, Ні)
+#     vet_passport TEXT NOT NULL, -- Ветеринарний паспорт (Так, Ні)
+#     image_path TEXT NOT NULL, -- Шлях до фотографії тваринки
+#     description TEXT NOT NULL -- Інформація про тваринку
+# )
+# ''')
+# Видалення всіх записів з таблиці
+cursor.execute('DELETE FROM users')
 
+# За потреби — скидання лічильника AUTOINCREMENT
+cursor.execute('DELETE FROM sqlite_sequence WHERE name="users"')
 # Підтвердження змін
 conn.commit()
 
